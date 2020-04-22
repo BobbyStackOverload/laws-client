dbde78f6b0c142dfe8e501e8dbdb86a494784af6
Bobby W Bryant <sirbob1999@gmail.com>
2020-04-22 10:32:01 -0400

    added new dependencies

---
+++ src/components/LawsList.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+    Link,
+  } from 'react-router-dom';
+
+class LawsList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        };
+    } 
+
+    async componentDidMount() {
+        const data = await this.generateLaws();
+        this.setState({
+          data
+        });
+      }
+    
+      generateLaws = async () => {
+        const url = "http://localhost:3000";
+        fetch (url)
+        .then(response => response.json())
+        .then(jsondata => this.setState({
+            data: jsondata
+        })) 
+        return this.state.data;
+      };
+
+    render() {
+        console.table(this.state.data)
+        if (this.state.data !== 0) {
+        const lawsData = this.state.data;
+
+        return (
+            <div>
+                <h1>Laws</h1>
+                <ul>
+                    {lawsData.map(element => (
+                        <div key={element.id}> <br></br>
+                        <Link to={`/state/${element.states}`}>{element.states} Laws</Link>
+                        </div>
+                    ))}
+                </ul>
+            </div>
+        )
+        } else {
+            return <div>Loading data....</div>
+        }
+    }
+}
+
+export default LawsList;
---
+++ src/components/StateLaw.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+
+class StateLaw extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+    } 
+
+    async componentDidMount() {
+        const data = await this.generateLaws();
+        this.setState({
+          data
+        });
+      }
+    
+      generateLaws = async () => {
+        const url = "http://localhost:3000";
+        fetch (url)
+        .then(response => response.json())
+        .then(jsondata => this.setState({
+            data: jsondata
+        })) 
+        return this.state.data;
+      };
+
+    render() {
+        if (this.state.data !== 0) {
+        const lawsData = this.state.data;
+        console.log("On the route law data", lawsData)
+            for (let i = 0; i < lawsData.length; i++) {
+                if (this.props.match.params.state === lawsData[i].states) {
+                    console.log("data on state",lawsData[i])
+        return (
+            <div className="lawPost">               
+                <h1>{this.props.match.params.state}</h1>            
+            </div>
+        )
+        } else {
+            return <div>Loading data....</div>
+        }
+    }
+}
+ }
+}
+export default StateLaw;
